17fd1558abe9cccc38630343c292d7aff05f526f
maximomei04 <maximo.mei04@gmail.com>
2025-11-21 23:28:39 -0300

    Update utilidades.py

--- utilidades.py
+++ utilidades.py
@@ -1,18 +1,17 @@
 import os, time
 
-
-def mostrar_matriz(matriz, encabezados="-" * 20):
+def mostrar_matriz(matriz, encabezados):
     print()
     for titulo in encabezados:
-        print(f"{titulo:<25}", end="")
+        print(f"{titulo:<20}", end="")
     print()
+    print("-" * (20 * len(encabezados)))
     for fila in matriz:
         for dato in fila:
-            print(f"{dato:<25}", end="")
+            print(f"{str(dato):<20}", end="")
         print()
     print()
     input("Presione ENTER para continuar")
-
 
 def limpiar_terminal(segundos=0):
     time.sleep(segundos)
@@ -21,48 +20,27 @@ def limpiar_terminal(segundos=0):
     else:
         os.system("clear")
 
-
 def ingreso_entero(mensaje="Ingrese un número entero: ", vacio=False):
-    """Solamente permite ingresar enteros positivos. Devuelve un int."""
     while True:
         valor_ingresado = input(mensaje).strip()
-
         if valor_ingresado == "" and vacio is True:
             return ""
-
         try:
             valor_entero = int(valor_ingresado)
-
             if valor_entero <= 0:
                 raise ValueError
-
             return valor_entero
-
         except ValueError:
-            print(
-                "Error: Ingreso inválido, solamente ingresar numeros enteros positivos"
-            )
-        except:
-            print("Error inesperado")
+            print("Error: Ingreso inválido, solamente ingresar numeros enteros positivos")
 
-
-def ingreso_texto(
-    mensaje="Ingrese un texto: ",
-    error="Ingreso inválido: No puede quedar vacío. Presione ENTER para reintentar",
-    vacio=False,
-):
-    """Opcionalmente permite ingresar strings vacíos"""
-    texto = ""
+def ingreso_texto(mensaje="Ingrese un texto: ", vacio=False):
     while True:
         texto = input(mensaje).strip()
         if texto == "" and vacio is False:
-            input(error)
+            print("No puede estar vacío.")
         else:
-            break
-    return texto
-
+            return texto
 
 def confirmacion(mensaje="Seguro? [Y/n]: "):
-    """Solicita confirmación, devuelve un True o False"""
     respuesta = input(mensaje).strip().lower()
     return respuesta in ("", "s")
